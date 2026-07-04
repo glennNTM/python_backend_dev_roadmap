c20215a84815a339bbb911031b570d7ebd502f7e
Glenn Ntoutoume <glenn.ntoutoume.dev@gmail.com>
2026-07-04 13:22:35 +0100

    la philosophie de python

---
+++ 1 - Fundamentals/Types Natifs/anagrammes.py
@@ -0,0 +1,4 @@
+# Fonction qui vérifie si deux chaînes sont des anagrammes l'une de l'autre (mêmes lettres, dans un ordre différent), sans utiliser la methode sorted(). Pour pimenter les choses
+
+def est_anagramme(string_1: str, string_2: str) -> bool:
+    pass
