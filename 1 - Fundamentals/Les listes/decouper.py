4dbcc5d93d3ecb869bb4048383972136386f06fd
Glenn Ntoutoume <glenn.ntoutoume.dev@gmail.com>
2026-07-07 17:08:17 +0100

    Ajout de la fonction decouper pour découper une liste en sous-listes de longueur spécifiée.

---
+++ 1 - Fundamentals/Les listes/decouper.py
@@ -0,0 +1,5 @@
+# Écris une fonction decouper(liste, taille) qui découpe une liste en sous-listes de longueur taille. 
+# La dernière sous-liste peut être plus courte si le nombre d'éléments n'est pas divisible par taille.
+
+def decouper(liste: list, taille: int):
+    pass
