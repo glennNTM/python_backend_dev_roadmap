03c3588226bf6ab58073b2abac0e40ec48e76ab8
Glenn Ntoutoume <glenn.ntoutoume.dev@gmail.com>
2026-07-09 20:18:57 +0100

    Ajout de la fonction regroupement pour regrouper les noms par département à partir d'une liste de tuples.

--- 1 - Fundamentals/Tuples/regroupement_par_cle.py
+++ 1 - Fundamentals/Tuples/regroupement_par_cle.py
@@ -1,1 +1,17 @@
-# 
+# Fonction qui prend une liste de tuples (nom, département) ([("Alice", "IT"), ("Bob", "RH"), ("Charlie", "IT")]) 
+# Retourne un dictionnaire regroupant les noms par département ({"IT": ["Alice", "Charlie"], "RH": ["Bob"]}). 
+def regroupement(liste: list[tuple])-> dict | None:
+    if not liste:
+        return None
+    resultat = {}
+
+    for nom, departement in liste:
+        if not departement in resultat:
+            # On ajoute le nouveau departement comme cle, la liste avec le nom en valeur
+            resultat[departement] = [nom]
+        else:
+            resultat[departement].append(nom)
+
+    return resultat
+
+print(regroupement([("Alice", "IT"), ("Bob", "RH"), ("Charlie", "IT")]))
