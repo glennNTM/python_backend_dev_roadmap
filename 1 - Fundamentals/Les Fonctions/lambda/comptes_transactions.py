af18c61e25c73881a6ad82ecb7a776f05355f97c
Glenn Ntoutoume <glenn.ntoutoume.dev@gmail.com>
2026-07-28 16:38:34 +0100

    Refactor sorting logic in comptes_transactions.py and script.py for clarity

--- 1 - Fundamentals/Les Fonctions/lambda/comptes_transactions.py
+++ 1 - Fundamentals/Les Fonctions/lambda/comptes_transactions.py
@@ -1,4 +1,12 @@
 # Objectif : obtenir les comptes triés du plus grand total de transactions au plus petit.
+
+"""""
+Tu as un dictionnaire {compte: [liste de transactions]}. 
+Trie les comptes selon la somme de leurs transactions, du plus grand total au plus petit, 
+en utilisant sorted() sur .items() avec une lambda qui calcule cette somme. Réfléchis à ce que reçoit ta lambda (un tuple clé-valeur) et comment y accéder.
+
+"""
+
 comptes_transactions = {
     "Alice": [15000, 22000, 8000],
     "Bob": [5000, 3000],
@@ -6,3 +14,6 @@ comptes_transactions = {
     "Diane": [12000],
 }
 
+comptes_transactions_tries = sorted(comptes_transactions.items(), key= lambda x: sum(x[1]), reverse=True)
+
+print(comptes_transactions_tries)
--- 1 - Fundamentals/Les Fonctions/lambda/script.py
+++ 1 - Fundamentals/Les Fonctions/lambda/script.py
@@ -8,6 +8,7 @@ comptes = [
     ("Emile", 47000),
 ]
 liste_trie = sorted(comptes, key=lambda x: x[0], reverse=True)
-max_comptes = max(comptes, key=lambda x: x[0])
+max_comptes = max(comptes, key=lambda x: x[1])
 
-print(liste_trie, max_comptes)
+print(liste_trie)
+print(max_comptes)
