b6f28ef4a49225a01e51ed49e77a61ff7a9445a8
Glenn Ntoutoume <glenn.ntoutoume.dev@gmail.com>
2026-08-06 12:52:50 +0100

    Refactor main.py to improve input handling and match case structure for menu options

--- 1 - Fundamentals/Projets/KwiziPay v0/main.py
+++ 1 - Fundamentals/Projets/KwiziPay v0/main.py
@@ -15,22 +15,24 @@ menu = """
 print("Bienvenue dans KwiziPay, vote gestionnaire de portefeuille Mobile Money (CLI). Quelle operation voulez-vous faire? : ")
 
 while True:
-    choix = int(input(menu))
-    if choix in [1, 2, 3, 4, 5, 6, 7]:
-        match choix:
-            case 1:
-                creer_un_compte()
-            case 2:
-                consulter_solde()
-            case 3:
-                deposer()
-            case 4:
-                retirer()
-            case 5:
-                transferer()
-            case 6:
-                historique()
-            case 7:
-                print("Bye")
-                break
-        
+    try:
+        choix = (input(menu))
+        if choix in ['1', '2', '3', '4', '5', '6', '7']:
+            match choix:
+                case '1':
+                    creer_un_compte()
+                case '2':
+                    consulter_solde()
+                case '3':
+                    deposer()
+                case '4':
+                    retirer()
+                case '5':
+                    transferer()
+                case '6':
+                    historique()
+                case '7':
+                    print("Bye")
+                    break
+    except KeyboardInterrupt:
+        break
